8d5a041b72030148e5cbb71cf94c58818e452eeb
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-10-21 21:31:20 -0300

    Laracast - Episode 3 - Homework

--- example/resources/views/components/layout.blade.php
+++ example/resources/views/components/layout.blade.php
@@ -9,9 +9,9 @@
 
 <body>
     <nav>
-        <a href="/">Home</a>
-        <a href="/about">about</a>
-        <a href="/contact">contact</a>
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
     </nav>
 
     {{ $slot }} <!-- Versão Curta -->
