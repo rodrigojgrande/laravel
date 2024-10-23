94e1925792bcc969b34c3129d91e36b616e2124a
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-10-22 23:00:38 -0300

    Laracast - Episode 06

--- example/resources/views/home.blade.php
+++ example/resources/views/home.blade.php
@@ -2,5 +2,5 @@
     <x-slot:heading>
         Home Page
     </x-slot:heading>
-    <h1>Hello from Home</h1>
+
 </x-layout>
--- example/routes/web.php
+++ example/routes/web.php
@@ -6,8 +6,50 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => 'R$50.000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => 'R$10.000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => 'R$40.000'
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => 'R$50.000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => 'R$10.000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => 'R$40.000'
+            ],
+        ];
+
+        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
