ddf0270c3ca6754f2378840349f7331a4c1e8da5
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-10-28 22:18:15 -0300

    Laracast - Episode 09

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -2,35 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all():array{
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 'R$50.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => 'R$10.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => 'R$40.000'
-            ],
-        ];
-    }
-    public static function find(int $id):array {
-        $job =  Arr::first(static::all(), fn($job) => $job['id'] == $id);
+class Job extends Model{
+    protected $table = 'job_listings';
 
-        if (! $job){
-            abort(404);
-        }
-    }
-
+    protected $fillable = ['title', 'salary'];
 }
 
