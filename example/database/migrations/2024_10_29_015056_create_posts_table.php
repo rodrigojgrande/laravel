9f8717545e42bc5cdd09a1e2abde71a268bf10c6
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-10-29 23:25:31 -0300

    Laracast - Episode 10

--- example/database/migrations/2024_10_29_015056_create_posts_table.php
+++ example/database/migrations/2024_10_29_015056_create_posts_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            // $table->unsignedBigInteger('employer_id');
-            $table->foreignIdFor(\App\Models\Employer::class);
-            $table->string('title');
-            $table->string('salary');
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('posts');
     }
 };
